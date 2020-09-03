b4249b4374e1fa4a91e67c411ae394d2e5077523
nikeo <nikeorever@gmail.com>
2020-09-03 17:47:40 +0800

    Add url launcher in flutter markdown page,optimize flutter page render

--- lib/common/theme.dart
+++ lib/common/theme.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 /// NAME       SIZE   WEIGHT   SPACING  2018 NAME
@@ -68,7 +69,10 @@ class NikeoLightTheme extends NikeoTheme {
   @override
   ThemeData get themeData => ThemeData(
         primarySwatch: white,
-        scaffoldBackgroundColor: white,
+        scaffoldBackgroundColor: Color.fromARGB(255, 249, 249, 249),
+        snackBarTheme: SnackBarThemeData(
+            contentTextStyle:
+                GoogleFonts.merriweatherSans(color: Colors.white)),
         fontFamily: GoogleFonts.merriweatherSans().fontFamily,
         floatingActionButtonTheme: const FloatingActionButtonThemeData(
             foregroundColor: Colors.teal, backgroundColor: Colors.white),
@@ -90,7 +94,8 @@ class NikeoLightTheme extends NikeoTheme {
       );
 
   @override
-  Animation<Color> get progressIndicatorValueColor => AlwaysStoppedAnimation(Colors.black54);
+  Animation<Color> get progressIndicatorValueColor =>
+      AlwaysStoppedAnimation(Colors.black54);
 
   @override
   DrawerHeaderThemeData get drawerHeaderThemeData => DrawerHeaderThemeData(
@@ -111,7 +116,9 @@ class NikeoDartTheme extends NikeoTheme {
   @override
   ThemeData get themeData => ThemeData(
       primarySwatch: Colors.teal,
-      scaffoldBackgroundColor: white,
+      scaffoldBackgroundColor: Color.fromARGB(255, 249, 249, 249),
+      snackBarTheme: SnackBarThemeData(
+          contentTextStyle: GoogleFonts.merriweatherSans(color: Colors.white)),
       fontFamily: GoogleFonts.merriweatherSans().fontFamily,
       floatingActionButtonTheme: const FloatingActionButtonThemeData(
           foregroundColor: Colors.white, backgroundColor: Colors.teal),
@@ -131,7 +138,8 @@ class NikeoDartTheme extends NikeoTheme {
               fontWeight: FontWeight.w400, fontSize: 14)));
 
   @override
-  Animation<Color> get progressIndicatorValueColor => AlwaysStoppedAnimation(Colors.teal);
+  Animation<Color> get progressIndicatorValueColor =>
+      AlwaysStoppedAnimation(Colors.teal);
 
   @override
   DrawerHeaderThemeData get drawerHeaderThemeData => DrawerHeaderThemeData(
@@ -141,4 +149,159 @@ class NikeoDartTheme extends NikeoTheme {
         subtitle2:
             themeData.textTheme.subtitle1.copyWith(color: Colors.white70),
       );
+}
+
+MarkdownStyleSheet markdownStyleSheet(BuildContext context) {
+  return MarkdownStyleSheet(
+    a: GoogleFonts.merriweatherSans(
+      // [google](http://www.google.com)
+      decoration: TextDecoration.underline,
+      color: Color.fromARGB(255, 12, 147, 228),
+      fontWeight: FontWeight.normal,
+      fontSize: 14,
+    ),
+    p: GoogleFonts.merriweatherSans(
+      // normal text
+      color: Color.fromARGB(255, 70, 70, 70),
+      fontWeight: FontWeight.normal,
+      fontSize: 14,
+    ),
+    code: GoogleFonts.merriweatherSans(
+      // `Dart`,
+      // ```dart
+      // void main() {}
+      // ```
+      backgroundColor: Color.fromRGBO(230, 230, 230, 1),
+      color: Color.fromARGB(255, 70, 70, 70),
+      fontWeight: FontWeight.normal,
+      fontSize: 14,
+    ),
+    codeblockPadding: EdgeInsets.all(8),
+    codeblockDecoration: BoxDecoration(
+      color: Color.fromRGBO(230, 230, 230, 1),
+      borderRadius: BorderRadius.circular(2.0),
+    ),
+    h1: GoogleFonts.merriweatherSans(
+      // # h1
+      color: Color.fromRGBO(0, 0, 0, 0.75),
+      fontWeight: FontWeight.bold,
+      fontSize: 24,
+    ),
+    h2: GoogleFonts.merriweatherSans(
+      // ## h2
+      color: Color.fromRGBO(0, 0, 0, 0.75),
+      fontWeight: FontWeight.bold,
+      fontSize: 20,
+    ),
+    h3: GoogleFonts.merriweatherSans(
+      // ### h3
+      color: Color.fromRGBO(0, 0, 0, 0.75),
+      fontWeight: FontWeight.bold,
+      fontSize: 16,
+    ),
+    h4: GoogleFonts.merriweatherSans(
+      // #### h4
+      color: Color.fromRGBO(0, 0, 0, 0.75),
+      fontWeight: FontWeight.bold,
+      fontSize: 14,
+    ),
+    h5: GoogleFonts.merriweatherSans(
+      // ##### h5
+      color: Color.fromRGBO(0, 0, 0, 0.75),
+      fontWeight: FontWeight.bold,
+      fontSize: 14,
+    ),
+    h6: GoogleFonts.merriweatherSans(
+      // ###### h6
+      color: Color.fromRGBO(0, 0, 0, 0.75),
+      fontWeight: FontWeight.bold,
+      fontSize: 14,
+    ),
+    em: GoogleFonts.merriweatherSans(
+      // *I*
+      fontStyle: FontStyle.italic,
+      color: Color.fromRGBO(0, 0, 0, 0.75),
+      fontWeight: FontWeight.normal,
+      fontSize: 14,
+    ),
+    strong: GoogleFonts.merriweatherSans(
+      // **B**
+      color: Color.fromRGBO(0, 0, 0, 0.75),
+      fontWeight: FontWeight.bold,
+      fontSize: 14,
+    ),
+    del: GoogleFonts.merriweatherSans(
+      // ~~D~~
+      color: Color.fromRGBO(0, 0, 0, 0.75),
+      decoration: TextDecoration.lineThrough,
+      fontWeight: FontWeight.normal,
+      fontSize: 14,
+    ),
+    img: GoogleFonts.merriweatherSans(
+      color: Color.fromRGBO(0, 0, 0, 0.5),
+      fontWeight: FontWeight.normal,
+      fontSize: 14,
+    ),
+    checkbox: GoogleFonts.merriweatherSans(
+      color: Theme.of(context).primaryColor,
+      decoration: TextDecoration.lineThrough,
+      fontWeight: FontWeight.normal,
+      fontSize: 14,
+    ),
+    tableHead: GoogleFonts.merriweatherSans(
+      color: Color.fromRGBO(0, 0, 0, 0.75),
+      fontWeight: FontWeight.bold,
+      fontSize: 14,
+    ),
+    tableBody: GoogleFonts.merriweatherSans(
+      // normal text
+      color: Color.fromARGB(255, 70, 70, 70),
+      fontWeight: FontWeight.normal,
+      fontSize: 14,
+    ),
+    tableHeadAlign: TextAlign.center,
+    tableBorder: TableBorder.all(
+      color: Color.fromARGB(255, 225, 225, 225),
+      width: 1,
+    ),
+    tableColumnWidth: const FlexColumnWidth(),
+    tableCellsPadding: const EdgeInsets.fromLTRB(16, 8, 16, 8),
+    tableCellsDecoration: const BoxDecoration(),
+    blockquote: GoogleFonts.merriweatherSans(
+      // > This is blockquote
+      color: Color.fromRGBO(0, 0, 0, 0.5),
+      fontWeight: FontWeight.normal,
+      fontSize: 14,
+    ),
+    blockquotePadding: const EdgeInsets.all(8),
+    blockquoteDecoration: BoxDecoration(),
+    blockquoteAlign: WrapAlignment.start,
+    blockSpacing: 8.0,
+    listIndent: 24.0,
+    listBullet: GoogleFonts.merriweatherSans(
+      /*
+
+                        * Item
+                          1. First Subitem
+                          2. Second Subitem
+                        * Item
+                          - Subitem
+                          - Subitem
+                        * Item
+
+                         */
+      color: Color.fromARGB(255, 70, 70, 70),
+      fontWeight: FontWeight.bold,
+      fontSize: 14,
+    ),
+    horizontalRuleDecoration: BoxDecoration(
+      // >---
+      border: Border(
+        top: BorderSide(
+          width: 1.0,
+          color: Color.fromARGB(255, 225, 225, 225),
+        ),
+      ),
+    ),
+  );
 }
--- lib/screens/reading.dart
+++ lib/screens/reading.dart
@@ -1,15 +1,21 @@
 import 'package:dartrofit/dartrofit.dart';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:posts_client/api/api.dart';
 import 'package:posts_client/api/http.dart';
+import 'package:posts_client/common/theme.dart';
 import 'package:posts_client/common/times.dart';
 import 'package:posts_client/models/Post.dart';
 import 'package:posts_client/models/nikeo_theme_model.dart';
 import 'package:timeconsuming_page_builder/timeconsuming_page_builder.dart';
 import 'package:provider/provider.dart';
 import 'package:wedzera/core.dart';
+import 'package:markdown/markdown.dart' as md;
+import 'package:url_launcher/url_launcher.dart';
 
 class MyReading extends StatelessWidget {
   MyReading({Key key, this.title}) : super(key: key);
@@ -41,13 +47,24 @@ class MyReading extends StatelessWidget {
             if (body.string.orEmpty().isEmpty) {
               return BuiltInEmptyWidget();
             }
-            return SafeArea(
-                child:
-                    Markdown(controller: controller, selectable: true, data: '''
+
+            final markdownData = '''
 ## ${post.title}
 #### ${monthShorthand(post.date.month)} ${post.date.day}, ${post.date.year}
+
 ${body.string}
-          '''));
+          ''';
+            return SafeArea(
+                child: Markdown(
+                    styleSheet: markdownStyleSheet(context),
+                    extensionSet: md.ExtensionSet.gitHubWeb,
+                    styleSheetTheme: MarkdownStyleSheetBaseTheme.material,
+                    onTapLink: (String href) async {
+                      await _launchURL(context, href);
+                    },
+                    controller: controller,
+                    selectable: true,
+                    data: markdownData));
           }),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.arrow_upward),
@@ -55,5 +72,14 @@ ${body.string}
             duration: Duration(seconds: 1), curve: Curves.easeOut),
       ),
     );
+  }
+
+  _launchURL(BuildContext context, String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      final snackbar = SnackBar(content: Text('Could not launch $url'));
+      Scaffold.of(context).showSnackBar(snackbar);
+    }
   }
 }
